7d361e7344ffef71d6fcba73b1687ddfa7ae6f94
M.Jahnavi <you@example.com>
2022-08-22 00:00:17 +0530

    abc

---
+++ Assignment/Assignment 2/Second.js
@@ -0,0 +1,13 @@
+function convert(min_Sec){
+    return (min_Sec*60);
+}
+console.log (convert(5));
+
+function Power(V,I){
+    return V*I;
+}
+console.log(Power(200,10));
+
+function Calculate_age(ageindays){
+    let year=day*365;
+}
