dd8a229fde22d3ced261a3e4fb4a1872af03426a
M.Jahnavi <you@example.com>
2022-09-12 19:54:25 +0530

    abc

--- reactapp/assessment/flask/dbms/cinnectdb.py
+++ reactapp/assessment/flask/dbms/cinnectdb.py
@@ -2,10 +2,13 @@ import mysql.connector
 myDb=mysql.connector.connect(host="127.0.0.1", user="root",
 password="Januravi962k1@",database="sql_store")
 myCursor=myDb.cursor()
-selectCommand="SELECT * FROM customers"
-myCursor.execute(selectCommand)
-result=myCursor.fetchall()
-for row in result:
-    print(row)
+#selectCommand="SELECT first_name, last_name FROM orders o JOIN customers c ON o.cutomer_id=c.customer_id"
+#selectCommand="CREATE TABLE app-users(id int, username text, password text)"
+appUser=(1,"jahnavi","abc")
+insertQuery="INSERT INTO appUsers VALUES (%s %s %s)"
+myCursor.execute(insertQuery,appUser)
+#result=myCursor.fetchall()
+#for row in result:
+   # print(f"{row[0]}{row[1]}")
 myDb.commit()
 myDb.close()
